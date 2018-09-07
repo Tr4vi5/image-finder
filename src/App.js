d1ed5fd113f5c3474241f11e0b167b24984d1ce1
Tr4vi5 <trav.dunn@outlook.com>
2018-09-07 00:01:34 -0500

    git initialized, origin added

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+import './App.css';
+import NavBar from './components/NavBar/NavBar.js'
+import Search from './components/Search/Search.js'
+
+class App extends Component {
+  render() {
+    return (
+      <MuiThemeProvider>
+        <div>
+          <NavBar />
+          <Search />
+        </div>
+      </MuiThemeProvider>
+    );
+  }
+}
+
+export default App;
